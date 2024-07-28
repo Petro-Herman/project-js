1d25b626039b62f08a9c667ee014ba79f0e95dd3
Petro-Herman <germanpetr24@gmail.com>
2024-07-29 00:18:29 +0300

    mob

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/css';
 
-var swiper = new Swiper('.projects-js', {
+const swiper = new Swiper('.projects-js', {
   slidesPerView: 1,
   spaceBetween: 30,
   keyboard: {
@@ -17,4 +17,15 @@ var swiper = new Swiper('.projects-js', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Tab') {
+    event.preventDefault(); 
+    if (event.shiftKey) {
+      swiper.slidePrev();
+    } else {
+      swiper.slideNext();
+    }
+  }
 });
