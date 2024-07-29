ff879cde24a9fadbba9164d3deb72d1aea45309b
Petro-Herman <germanpetr24@gmail.com>
2024-07-29 23:49:00 +0300

    п

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/css';
 
-const swiper = new Swiper('.projects-js', {
+export const swiper = new Swiper('.projects-js', {
   slidesPerView: 1,
   spaceBetween: 30,
   keyboard: {
